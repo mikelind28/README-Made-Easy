623985be777e408a34fc241e895b22b3f1083bf7
Mike Lind <mikelind28@gmail.com>
2024-10-21 16:36:52 -0500

    updated comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// a function that returns a license badge based on which license the user selects. if no license is selected, returns an empty string.
 function renderLicenseBadge(license) {
   if (license === "MIT") {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
@@ -18,19 +17,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-/*
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-*/
-
-// TODO: Create a function to generate markdown for README
+// a function to generate markdown content for the README.
 function generateMarkdown(project) {
   return `# ${project.title}
 
